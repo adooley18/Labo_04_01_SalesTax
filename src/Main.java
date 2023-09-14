15a66c5304fd0b400d7dfffc7462ffa942ac795a
ajdooley18 <ajdooley16@gmail.com>
2023-09-14 17:24:35 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int purchasePrice = 100;
+        double salesTax = purchasePrice * 0.05;
+        System.out.println("Your sales tax on a purchase of " + "$" + purchasePrice + " is " + "$" + salesTax + "!");
+    }
+}
